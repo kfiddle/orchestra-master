77f0bbd8e66e80fceb65667599250677d4d026d3
kfiddle <kenjfiddle@gmail.com>
2021-09-15 16:41:56 -0400

    date formatter

--- src/components/performances/Performance.js
+++ src/components/performances/Performance.js
@@ -1,13 +1,14 @@
-
+import DateFormatter from '../helperFunctions/DateFormatter';
 
 import styles from './Performance.module.css';
 
 const Performance = props => {
     const { title, date } = props.performance;
+    const displayDate = DateFormatter(date);
 
     return <div className={styles.outerContainer}>
         <div className={styles.titleDiv}>{title}</div>
-        <div className={styles.dateDiv}>{date}</div>
+        <div className={styles.dateDiv}>{displayDate}</div>
     </div>
 
 
