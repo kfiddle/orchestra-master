b88ad2c37b26641b67750eaa71e92eb933da899b
kfiddle <kenjfiddle@gmail.com>
2021-09-15 19:27:00 -0400

    various

--- src/components/performances/Performance.js
+++ src/components/performances/Performance.js
@@ -1,10 +1,15 @@
-import { FiEdit } from "react-icons/fi";
+import { useState } from "react";
+
+import { FiEdit, FiPlus } from "react-icons/fi";
 
 import DateFormatter from "../helperFunctions/DateFormatter";
+import PieceEntry from "../pieceEntry/PieceEntry";
 
 import styles from "./Performance.module.css";
 
 const Performance = (props) => {
+  const [addPieceClicked, setAddPieceClicked] = useState(false);
+
   const { title, date } = props.performance;
   const displayDate = DateFormatter(date);
 
@@ -12,13 +17,24 @@ const Performance = (props) => {
     console.log(props.performance);
   };
 
+  const closeModal = () => {
+      setAddPieceClicked(false);
+  }
+
+  const openAddPieceModal = () => {
+      setAddPieceClicked(true);
+  }
+
   return (
-    <div className={styles.outerContainer} onClick={()=> console.log('test')}>
+    <div className={styles.outerContainer} onClick={() => console.log("test")}>
       <div className={styles.titleDiv}>{title}</div>
       <div className={styles.dateDiv}>{displayDate}</div>
-      <div className={styles.editButtonDiv}>
+
+      <div className={styles.buttonsDiv}>
+        <FiPlus className={styles.plusSign} onClick={openAddPieceModal} />
         <FiEdit onClick={editPerformance} className={styles.editButton} />
       </div>
+      {addPieceClicked && <PieceEntry closeModal={closeModal}/>}
     </div>
   );
 };
