46b967f8b5ea3a84c81e84619b4c7f38a1ca2ebe
kfiddle <kenjfiddle@gmail.com>
2021-10-01 15:48:40 -0400

    various

--- src/components/piece/Roster.js
+++ src/components/piece/Roster.js
@@ -1,8 +1,9 @@
 import RosterSpot from "./RosterSpot";
 
 const Roster = (props) => {
+
   const displayableRosterSpots = props.roster.map(ppp => (
-    <RosterSpot key={ppp.id} instrument={ppp.instrumentEnum} />
+    <RosterSpot key={ppp.id} instrument={ppp.instrumentEnum} player={ppp.player? ppp.player.lastName: ''} />
   ))
 
   return <div>{displayableRosterSpots}</div>;
--- src/components/piece/RosterSpot.js
+++ src/components/piece/RosterSpot.js
@@ -3,7 +3,7 @@
 import classes from './RosterSpot.module.css';
 const RosterSpot = props => {
 
-return <div className={classes.outerContainer}>{props.instrument}</div>
+return <div className={classes.outerContainer}>{props.instrument} {props.player}</div>
 
 };
 
