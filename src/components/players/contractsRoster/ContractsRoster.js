f24eb911ac0b4678d0eda9985ba0484afe442726
kfiddle <kenjfiddle@gmail.com>
2021-11-07 15:14:47 -0500

    Various

--- src/components/players/contractsRoster/ContractsRoster.js
+++ src/components/players/contractsRoster/ContractsRoster.js
@@ -1,14 +1,25 @@
+import Player from "../Player";
+
 import classes from "./ContractsRoster.module.css";
 
 const ContractsRoster = (props) => {
   const playersList = props.list;
 
-  const displayableList = playersList.map(player => (
-      <div key={player.id} className={classes.playerContainer}>{player.lastName}</div>
-  ))
+  const clickedPlayerHandler = () => {
+    console.log("hello");
+  };
 
+  const displayableList = playersList.map((player) => (
+    <Player key={player.id} player={player} clicked={clickedPlayerHandler}>
+      {player.lastName}
+    </Player>
+  ));
 
-  return <div className={classes.outerContainer}>{displayableList}</div>
+  return (
+    <div className={classes.outerContainer}>
+      <div className={classes.innerContainer}>{displayableList}</div>
+    </div>
+  );
 };
 
 export default ContractsRoster;
--- src/components/players/playerEntry/ExtraTypeBox.js
+++ src/components/players/playerEntry/ExtraTypeBox.js
@@ -1,34 +1,59 @@
 import { useContext, useEffect, useState } from "react";
 
 import InstrumentsList from "../../../store/instruments-list";
+import BigInput2 from "../../input/BigInput2";
 
 import classes from "./ExtraType.module.css";
 
-const violins = ["Section", "Associate", "Principal", "Concertmaster"];
+const winds = ["Principal", "2", "3", "4"];
+const trumpets = ["Principal", "2", "3", "4"];
+const horns = ["Principal", "Assistant", "2", "3", "4"];
+const trombones = ["Principal", "2", "3"];
+const twos = ["Principal", "2"];
+
+const violin1 = ["Concertmaster", "Associate", "Section"];
 const otherStrings = ["Section", "Associate", "Principal"];
-const horns = ["Principal", "Assistant", "Second", "Third", "Fourth"];
-const trumpets = ["Principal", "Assistant", "Second", "Third"];
 const allElse = ["Section", "Associate", "Principal"];
 
 const ExtraTypeBox = (props) => {
   const { clickedInstrumentList } = useContext(InstrumentsList);
   const [whichContracts, setWhichContracts] = useState([]);
+
   const contracted = props.contracted;
+
+  const state = { player: props.player, setPlayer: props.setter };
 
   useEffect(() => {
     if (clickedInstrumentList.length > 0) {
-      if (clickedInstrumentList[0].name === "Violin") {
-        setWhichContracts(violins);
-      } else if (clickedInstrumentList[0].name === "Trumpet") {
-        setWhichContracts(trumpets);
-      } else if (clickedInstrumentList[0].name === "Horn") {
-        setWhichContracts(horns);
+      if (
+        clickedInstrumentList[0].name === "Flute" ||
+        clickedInstrumentList[0].name === "Oboe" ||
+        clickedInstrumentList[0].name === "Clarinet" ||
+        clickedInstrumentList[0].name === "Bassoon"
+      ) {
+        setWhichContracts(winds);
+      } else if (clickedInstrumentList[0].name === "Trombone") {
+        setWhichContracts(trombones);
       } else if (
+        clickedInstrumentList[0].name === "Harp" ||
+        clickedInstrumentList[0].name === "Keyboard" ||
+        clickedInstrumentList[0].name === "Timpani" ||
+        clickedInstrumentList[0].name === "Tuba"
+      ) {
+        setWhichContracts(twos);
+      } else if (clickedInstrumentList[0].name === "Violin 1") {
+        setWhichContracts(violin1);
+      } else if (
+        clickedInstrumentList[0].name === "Violin2" ||
         clickedInstrumentList[0].name === "Viola" ||
         clickedInstrumentList[0].name === "Cello" ||
         clickedInstrumentList[0].name === "Bass"
       ) {
         setWhichContracts(otherStrings);
+      } else if (clickedInstrumentList[0].name === "Trumpet") {
+        setWhichContracts(trumpets);
+      } else if (clickedInstrumentList[0].name === "Horn") {
+        setWhichContracts(horns);
       } else {
         setWhichContracts(allElse);
       }
@@ -40,7 +65,7 @@ const ExtraTypeBox = (props) => {
     <button
       key={whichContracts.indexOf(contract)}
       className={classes.contractButton}
-      type={'button'}
+      type={"button"}
     >
       {contract}
     </button>
@@ -48,20 +73,35 @@ const ExtraTypeBox = (props) => {
 
   const subBox = (
     <div className={classes.subBox}>
-      <button className={classes.subButton} type={'button'}>A</button>
-      <button className={classes.subButton} type={'button'}>B</button>
-      <button className={classes.subButton} type={'button'}>C</button>
-      <button className={classes.subButton} type={'button'}>D</button>
+      <button className={classes.subButton} type={"button"}>
+        A
+      </button>
+      <button className={classes.subButton} type={"button"}>
+        B
+      </button>
+      <button className={classes.subButton} type={"button"}>
+        C
+      </button>
+      <button className={classes.subButton} type={"button"}>
+        D
+      </button>
     </div>
   );
 
   const contractBox = (
     <div className={classes.contractBox}>
       {displayableContracts}
-
-      {/* <button className={classes.contractButton}>Principal</button>
-      <button className={classes.contractButton}>Assistant</button>
-      <button className={classes.contractButton}>Section</button> */}
+      {/* <div className={classes.contractButtonsDiv}>{displayableContracts}</div> */}
+      {/* <div className={classes.contractInputBox}>
+        <BigInput2
+          shebang={{
+            label: "Contract Title",
+            key: "contractTitle",
+            state,
+            style: { width: "80%" },
+          }}
+        />
+      </div> */}
     </div>
   );
 
