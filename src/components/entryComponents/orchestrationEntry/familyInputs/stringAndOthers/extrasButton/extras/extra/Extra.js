af8e7b0cb6c12c4305d7221311ca0a3f1d9df6b4
kfiddle <kenjfiddle@gmail.com>
2022-02-26 11:37:59 -0500

    Various

--- src/components/entryComponents/orchestrationEntry/familyInputs/stringAndOthers/extrasButton/extras/extra/Extra.js
+++ src/components/entryComponents/orchestrationEntry/familyInputs/stringAndOthers/extrasButton/extras/extra/Extra.js
@@ -1,15 +1,26 @@
+import { useState } from "react";
+import { Fragment } from "react/cjs/react.production.min";
+
 import styles from "./Extra.module.css";
 
+const originalBackground =
+  "linear-gradient(to left, transparent, #eccbcb, rgb(90, 85, 85)";
+
 const Extra = (props) => {
-  console.log(props.instrument);
+  const [clicked, setClicked] = useState(false);
 
   const clickHandler = () => {
-    console.log(props.instrument);
+    setClicked((previous) => !previous);
   };
 
+  let classNames = !clicked ? styles.instrumentItemDiv : styles.clickedItem;
+
   return (
-    <div onClick={clickHandler} className={styles.instrumentItemDiv}>
-      <div className={styles.nameDiv}>{props.instrument}</div>
+    <div className={styles.outerContainer}>
+      <div onClick={clickHandler} className={classNames}>
+        <div className={styles.nameDiv}>{props.instrument}</div>
+      </div>
+      {clicked && <div style={{ color: "red" }}>howdy</div>}
     </div>
   );
 };
