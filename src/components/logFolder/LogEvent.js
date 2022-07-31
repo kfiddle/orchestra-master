6d498eeaf76967ccd301664bce07814bd14d5888
kfiddle <kenjfiddle@gmail.com>
2022-07-31 17:07:52 -0400

    more on log events

--- src/components/logFolder/LogEvent.js
+++ src/components/logFolder/LogEvent.js
@@ -1,8 +1,39 @@
+import { ImCross } from "react-icons/im";
+
+import { ImCheckmark } from "react-icons/im";
+import { FaQuestion } from "react-icons/fa";
+
 import styles from "./LogEvent.module.css";
 
 const LogEvent = ({ logEvent }) => {
+  const { date, gigOffer } = logEvent;
+  const { player, show, reply } = gigOffer;
+
   console.log(logEvent);
-  return <div>{logEvent.date}</div>;
+
+  let replyMark;
+
+  switch (reply) {
+    case "ACCEPT":
+      replyMark = <ImCheckmark className={styles.checkIcon} />;
+      break;
+    case "DECLINE":
+      replyMark = <ImCross className={styles.xIcon} />;
+      break;
+    case "MAYBE":
+      replyMark = <FaQuestion className={styles.questionIcon} />;
+      break;
+  }
+
+  return (
+    <div className={styles.outerContainer}>
+      <div className={styles.nameDiv}>
+        {player.firstNameArea} {player.lastName}
+      </div>
+      <div className={styles.showTitleDiv}>{show.title}</div>
+      <div className={styles.replyDiv}>{replyMark}</div>
+    </div>
+  );
 };
 
 export default LogEvent;
