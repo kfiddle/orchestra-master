3430055e02c2c596f464eac3b3a5134b6d7bdb76
kfiddle <kenjfiddle@gmail.com>
2022-09-16 13:12:35 -0400

    putting email in place

--- src/components/console/possiblesBox/possible/Possible.js
+++ src/components/console/possiblesBox/possible/Possible.js
@@ -1,21 +1,34 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 
 import useFetch from "../../../../hooks/useFetch";
 
+import emailjs from "emailjs-com";
+import { AiOutlineMail } from "react-icons/ai";
+
 import { ChairsHolder, ConsoleHolder } from "../../../../store/object-holder";
+
+import EmailPlayer from "../../rosterBox/rosterSpot/emailPlayer/EmailPlayer";
 
 import classes from "./Possible.module.css";
 
-const Possible = (props) => {
+const Possible = ({ player }) => {
   const { chairState, dispatch: chairsDispatch } = useContext(ChairsHolder);
   const { dashboard, dispatch: dashDispatch } = useContext(ConsoleHolder);
+  const [mailClicked, setMailClicked] = useState(false);
 
   const pusher = useFetch();
 
-  const player = props.player;
   const { firstNameArea, lastName } = player;
 
   let outerContainerClass = classes.unclickedItem;
+
+  const sendMessage = () => {
+    setMailClicked(true);
+  };
+
+  const closeModal = () => {
+    setMailClicked(false);
+  };
 
   const doubleClickHandler = async () => {
     let response = await pusher(
@@ -36,6 +49,10 @@ const Possible = (props) => {
       <div className={classes.nameDiv}>
         {firstNameArea} {lastName}{" "}
       </div>
+      <div className={classes.mailButtonDiv}>
+        <AiOutlineMail className={classes.icon} onClick={sendMessage} />
+      </div>
+      {mailClicked && <EmailPlayer closeModal={closeModal} player={player} />}
     </div>
   );
 };
--- src/components/console/rosterBox/rosterSpot/emailPlayer/EmailPlayer.js
+++ src/components/console/rosterBox/rosterSpot/emailPlayer/EmailPlayer.js
@@ -1,13 +1,34 @@
 import Modal from "../../../../UI/modal/Modal";
 
-import emailJS from "emailjs-com";
+import emailjs from "emailjs-com";
 
 import styles from "./EmailPlayer.module.css";
 
-const EmailPlayer = (props) => {
+const userId = "user_ziX5oSLNJRahUxs9dz2xC";
+const serviceId = "service_whc7i1l";
+// const templateId = "template_xhux42i";
+const testTemplateId = "template_38pylf2";
+
+const EmailPlayer = ({ closeModal, player }) => {
+  const submit = () => {
+    const testContact = {
+      toEmail: player.email,
+      message:
+        "If anyone actually gets this, it means I can email from pushing a button on this app",
+    };
+
+    emailjs.send(serviceId, testTemplateId, testContact, userId);
+  };
   return (
-    <Modal closeModal={props.closeModal}>
-      <div>These players are nutso</div>
+    <Modal closeModal={closeModal}>
+      <div className={styles.outerContainer}>
+        These players are nutso
+        <div className={styles.submitButtonDiv}>
+          <button className={styles.button} onClick={submit}>
+            SUBMIT
+          </button>
+        </div>
+      </div>
     </Modal>
   );
 };
