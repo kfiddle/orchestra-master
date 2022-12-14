1df25eaedd7cfd7fdf80f1cc20fb0a39e4bdf504
kfiddle <kenjfiddle@gmail.com>
2022-12-14 09:14:22 -0500

    firebase build and also typo in whichServer

--- src/components/helperFunctions/WhichServer.js
+++ src/components/helperFunctions/WhichServer.js
@@ -1,11 +1,12 @@
 const WhichServer = () => {
   // former was ... https://morning-meadow-84570.herokuapp.com/
   // version 2 ... https://mighty-bayou-32694.herokuapp.com/
+  // "https://glacial-headland-13646.herokuapp.com/"
 
-  const realServer = "https://glacial-headland-13646.herokuapp.com/";
+  const realServer = "https://aqueous-hollows-69418.herokuapp.com/";
   const fakeServer = "http://localhost:8080/";
 
-  return fakeServer;
+  return realServer;
 };
 
 export default WhichServer;
