4c5084010f419073f9422e5cedc067d50c8a24f1
kfiddle <kenjfiddle@gmail.com>
2023-10-19 09:32:18 -0400

    small logic tweak resetting dashboard in mainConsole

--- src/components/console/MasterConsole5.js
+++ src/components/console/MasterConsole5.js
@@ -1,23 +1,23 @@
-import { useEffect, useContext } from "react";
-import { useSelector } from "react-redux";
+import { useEffect, useContext } from 'react';
+import { useSelector } from 'react-redux';
 
-import Shows from "./shows/Shows";
-import Pieces from "./pieces/Pieces";
+import Shows from './shows/Shows';
+import Pieces from './pieces/Pieces';
 
 import showPieces from '../../dummyData/showPieces';
-import chairs from "../../dummyData/chairs";
+import chairs from '../../dummyData/chairs';
 
-import { ConsoleHolder } from "../../store/object-holder";
+import { ConsoleHolder } from '../../store/object-holder';
 
-import RosterBox from "./rosterBox/RosterBox";
-import useFetch from "../../hooks/useFetch";
-import useRequestMapping from "../../hooks/useRequestMapping";
+import RosterBox from './rosterBox/RosterBox';
+import useFetch from '../../hooks/useFetch';
+import useRequestMapping from '../../hooks/useRequestMapping';
 
 //season2 has this
 
-import styles from "./MasterConsole5.module.css";
-import useGetAList2 from "../../hooks/useGetAList2";
-import useFullOrch from "../../hooks/useFullOrch";
+import styles from './MasterConsole5.module.css';
+import useGetAList2 from '../../hooks/useGetAList2';
+import useFullOrch from '../../hooks/useFullOrch';
 
 const MasterConsole5 = (props) => {
   const { dashboard, dispatch } = useContext(ConsoleHolder);
@@ -31,32 +31,30 @@ const MasterConsole5 = (props) => {
     //   "get-showtunes-on-program"
     // );
 
-    const showPieces_On_Show = showPieces.filter(showPiece => showPiece.showId === dashboard.clickedShowId)
-    if (showPieces_On_Show.length) dispatch({ type: "showPieces", list: showPieces_On_Show });
+    const showPieces_On_Show = showPieces.filter((showPiece) => showPiece.showId === dashboard.clickedShowId);
+    if (showPieces_On_Show.length) dispatch({ type: 'showPieces', list: showPieces_On_Show });
   };
 
   const grabPICSFromShow = async () => {
     // const directPICS = await pusher(dashboard.clickedShow, "get-pics-in-show");
-    const directPICS = chairs.filter(chair => chair.showId === dashboard.clickedShowId);
-    if (directPICS.length) dispatch({ type: "pics", list: directPICS });
-    
+    const directPICS = chairs.filter((chair) => chair.showId === dashboard.clickedShowId);
+    if (directPICS.length) dispatch({ type: 'pics', list: directPICS });
   };
 
   useEffect(() => {
     if (dashboard.clickedShowId || dashboard.modalClosed) {
-
-      dispatch({ type: "pics", list: [] });
-      console.log(dashboard)
+      dispatch({ type: 'pics', list: [] });
+      dispatch({ type: 'showPieces', list: [] });
       grabThePieces();
       grabPICSFromShow();
-      dispatch({ type: "modalClosed", modalClosed: false });
+      dispatch({ type: 'modalClosed', modalClosed: false });
     }
   }, [dashboard.clickedShowId, dashboard.modalClosed]);
 
   useEffect(() => {
     if (dashboard.refreshPICS) {
       grabPICSFromShow();
-      dispatch({ type: "refreshPICS", refreshPICS: false });
+      dispatch({ type: 'refreshPICS', refreshPICS: false });
     }
   }, [dashboard.refreshPICS]);
 
